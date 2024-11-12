bba449578873a4c499225b8908b72083043033d3
Mehmet Nadir Dere <mehmet.dere@lernende.bbw.ch>
2024-11-12 09:33:56 +0100

    commit

--- 6.1/1.7/script.js
+++ 6.1/1.7/script.js
@@ -1,41 +1,38 @@
-const guessGame = () => {
-  alert("I chose a number between 1 and 100, so you have to guess it now!");
-
-  
-};
-
 const form = document.forms.form
+
+let newGame = true;
+
+alert("I chose a number between 1 and 100, so you have to guess it now!");
+
+let numberToGuess = Math.floor(Math.random() * 100) + 1;
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const numberField = form.elements.number;
+  if (newGame) {
+    numberToGuess = Math.floor(Math.random() * 100) + 1;
+    newGame = false
+  }
 
-  const numberToGuess = Math.floor(Math.random() * 100) + 1;
+  let numberField = parseInt(form.elements.number.value);
 
-  let responseNumber;
-
-  let shouldGameContinue = true;
-
-  while (shouldGameContinue) {
-    numberField = form.elements.number;;
-    if (!isNaN(responseNumber)) {
-        if (responseNumber > 100 || responseNumber < 0) { 
+    if (!isNaN(numberField)) {
+        if (numberField > 100 || numberField < 0) { 
             alert("The number has to be between 1 and 100!")
         } else {
-            if (responseNumber < numberToGuess) {
+            if (numberField < numberToGuess) {
                 alert("Too low!");
-              } else if (responseNumber > numberToGuess) {
+              } else if (numberField > numberToGuess) {
                 alert("Too high!");
               } else {
                 alert(`You won! The number was ${numberToGuess}`)
-                shouldGameContinue = false;
+                alert("I chose a new number between 1 and 100, so you have to guess it now!");
+                newGame = true;
               }
         }
     } else {
         alert("It has to be a number!")
     }
-  }
 })
 
 // nicht fertig
